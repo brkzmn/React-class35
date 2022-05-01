e1ebe60f86187c619dd75d195d7772240f944ccb
Burak Özmen <brkzmn.wd@gmail.com>
2022-05-02 00:57:43 +0200

    modified

---
+++ week3/project/ecommerce-folder/src/components/ProductCard.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import FavouriteButton from "./FavouriteButtton";
+
+function ProductCard({ productInfo }) {
+  return (
+    <li className="product-card">
+      <Link to={`/product/${productInfo.id}`}>
+        <div>
+          <img
+            className="product-image"
+            src={productInfo.image}
+            alt={productInfo.title}
+          />
+
+          <span className="product-title">{productInfo.title}</span>
+        </div>
+      </Link>
+      <FavouriteButton id={productInfo.id} />
+    </li>
+  );
+}
+
+export default ProductCard;
